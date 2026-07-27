0af8189657be0e1d451c779753ca5bb3340c0782
Mehroof Saba <mehroofsaba2@gmail.com>
2026-07-27 23:17:24 +0530

    Add opening book support for AI

---
+++ src/main/java/com/mehroof/chessmaster/ai/book/OpeningBook.java
@@ -0,0 +1,28 @@
+package com.mehroof.chessmaster.ai.book;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.mehroof.chessmaster.ai.AIMove;
+import com.mehroof.chessmaster.ai.hash.ZobristHash;
+import com.mehroof.chessmaster.model.BoardState;
+
+public class OpeningBook {
+
+    private final Map<Long, AIMove> openings =
+            new HashMap<>();
+
+    public OpeningBook() {
+
+    }
+
+    public AIMove getBookMove(BoardState board) {
+
+        long hash =
+                ZobristHash.computeHash(board);
+
+        return openings.get(hash);
+
+    }
+
+}
